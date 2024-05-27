ca3b111304b6471d90bd4c744d3b79919d65cd6f
theskid22 <shrey150436@gmail.com>
2024-05-27 15:37:25 -0400

    fixed a weird bug with CSS, added ethandupe

---
+++ ethandupe/chat/README.md
@@ -0,0 +1,10 @@
+<p align="center">
+  <img src="https://raw.githubusercontent.com/red-stone-network/muenster/images/images/mnstr.png" align="center">
+</p>
+
+<p align="center">
+  The client for Muenster, easily deployable.<br>
+  All you need to do is deploy this to a website.<br>
+  
+  Muenster's client can be hosted almost anywhere - it should work on Netlify, Vercel, Stormkit, Glitch, Replit, Render, and other services.
+</p>
---
+++ scripts/navigation.js
@@ -0,0 +1,20 @@
+function goto(area) {
+            if (area == 'games') {
+                window.location.href = "./games.html";
+            } else if (area == "star") {
+                window.location.href = './star/index.html';
+            } else if (area == 'resources') {
+                window.location.href= './resources.html';
+            } else if (area == 'home') {
+                window.location.href = './index.html';
+            } else if (area == 'about') {
+                window.location.href = './about.html';
+            } else if (area == 'news') {
+                window.location.href = './news.html';
+            } else if (area == 'discord') {
+                window.location.href = 'https://discord.gg/yJUGGs8EAA';
+            }
+			 else if (area == 'ethandupe') {
+			 window.location.href = './ethandupe/index.html'
+			 }
+}
