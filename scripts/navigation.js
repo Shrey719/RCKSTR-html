3f6f6ba7bd5e358a763ad1c8dd45d4c16027e69c
29henbar29 <kd9ywf@outlook.com>
2024-05-30 09:19:26 -0500

    Update navigation.js

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,20 +1,21 @@
 function goto(area) {
-            if (area == 'games') {
-                window.location.href = "./games.html";
-            } else if (area == "star") {
-                window.location.href = './star/index.html';
-            } else if (area == 'resources') {
-                window.location.href= './resources.html';
-            } else if (area == 'home') {
-                window.location.href = './index.html';
-            } else if (area == 'about') {
-                window.location.href = './about.html';
-            } else if (area == 'news') {
-                window.location.href = './news.html';
-            } else if (area == 'discord') {
-                window.location.href = 'https://discord.gg/yJUGGs8EAA';
-            } else if (area == 'ethandupe') {
-		window.location.href = './ethandupe/index.html'
-	    } else if (area == "Math Solver") {
-                window.location.href = './mathsolver/index.html';
+    if (area == 'games') {
+        window.location.href = "./games.html";
+    } else if (area == "star") {
+        window.location.href = './star/index.html';
+    } else if (area == 'resources') {
+        window.location.href= './resources.html';
+    } else if (area == 'home') {
+        window.location.href = './index.html';
+    } else if (area == 'about') {
+        window.location.href = './about.html';
+    } else if (area == 'news') {
+        window.location.href = './news.html';
+    } else if (area == 'discord') {
+        window.location.href = 'https://discord.gg/yJUGGs8EAA';
+    } else if (area == 'ethandupe') {
+        window.location.href = './ethandupe/index.html';
+    } else if (area == 'mathsolver') {
+        window.location.href = './mathsolver/index.html';
+    }
 }
