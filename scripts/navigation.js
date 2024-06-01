d05790b20942da65afb9fc6ec668f220d332d455
theskid22 <shrey150436@gmail.com>
2024-06-01 19:23:46 -0400

    actualy fixed

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,24 +1,24 @@
 function goto(area) {
     if (area == 'games') {
-        window.location.href = "./games.html";
+        window.location.href = "/games.html";
     } else if (area == "star") {
-        window.location.href = './star/index.html';
+        window.location.href = '/star/index.html';
     } else if (area == 'resources') {
-        window.location.href= './resources.html';
+        window.location.href= '/resources.html';
     } else if (area == 'home') {
-        window.location.href = './index.html';
+        window.location.href = '/index.html';
     } else if (area == 'about') {
-        window.location.href = './about.html';
+        window.location.href = '/about.html';
     } else if (area == 'news') {
-        window.location.href = './news.html';
+        window.location.href = '/news.html';
     } else if (area == 'discord') {
         window.location.href = 'https://discord.gg/yJUGGs8EAA';
     } else if (area == 'ethandupe') {
-        window.location.href = './ethandupe/index.html';
+        window.location.href = '/ethandupe/index.html';
     } else if (area == 'mathsolver') {
-        window.location.href = './mathsolver/index.html';
+        window.location.href = '/mathsolver/index.html';
     } else if (area == 'settings') {
-        window.location.href = './settings.html';
+        window.location.href = '/settings.html';
     } else if (area == 'interstellar') {
         window.location.href = 'https://poor-kiri-rckstr-32bc0b60.koyeb.app/';
     } else if (area == 'emulator') {
