510be63d63eeffe91fa2e0c69ef9746b5e73c78e
theskid22 <shrey150436@gmail.com>
2024-05-31 23:40:48 -0400

    added some more colors

--- settings.js
+++ settings.js
@@ -20,6 +20,18 @@ if (getCookie("theme") == 'white') {
 	document.body.style.background = "white"
 	document.body.style.color = "black"
 }
+if (getCookie("theme") == 'pink') {
+    document.body.style.background = "#f2b8e6"
+    document.body.style.color = "#fa4848"
+}
+if (getCookie("theme") == 'blue'){
+    document.body.style.background = "#b8e6f2"
+    document.body.style.color = "#4848fa"
+}
+if (getCookie("theme") == 'red'){
+    document.body.style.background = "#edabdf"
+    document.body.style.color = "#f55858"
+}
 // Just realised that this is 100% useless
 //if (getCookie("theme") == "def") { 
 	//document.body.style.background = "black"
